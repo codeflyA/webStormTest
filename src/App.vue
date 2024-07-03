acc569fa6d474b372ac2533c695a3f5859c0447f
胡天琦 <524176336@qq.com>
2024-07-03 16:51:11 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import {ref} from "vue";
+import {ElTree} from "element-plus";
+
+const personnelTreeRef = ref<InstanceType<typeof ElTree>>()
+const defaultProps = {
+  children: 'children',
+  label: 'name',
+  isLeaf: 'isLeaf',
+}
+
+function handleNodeClick() {
+}
+
+function loadNode() {
+}
+</script>
+
+<template>
+  <el-tree
+      ref="personnelTreeRef"
+      :load="loadNode"
+      :props="defaultProps"
+      lazy
+      node-key="id"
+      @node-click="handleNodeClick"
+  >
+  </el-tree>
+</template>
+
+<style scoped>
+
+</style>
